695213980745a88856cb4e9cb16af67a312912f7
Joe Cormier <jncormier24@gmail.com>
2013-03-25 09:54:48 -0400

    Added files for HW5 and project 3

---
+++ projects/project_3/project3.java
@@ -0,0 +1,12 @@
+/**
+ * Project 3
+ * By: Joe Cormier
+ * This project is intended to be a traffic monitor. It will determine the lenght of time a light will stay green at a 1 way
+ * tunnel.
+ * */
+
+public class project3{
+	public static void main(String[] args){
+		System.out.println("Traffic Monitor.");
+	}
+}
